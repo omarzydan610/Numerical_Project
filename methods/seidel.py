30b9d05c0b42dc1ac75edc093b61d1e3b2fc5896
ShahdMohamed-11 <shahdmohamed7377@gmail.com>
2024-11-27 15:35:27 +0200

    add steps to seidel.py

--- methods/seidel.py
+++ methods/seidel.py
@@ -1,4 +1,3 @@
-
 import time
 class Seidel:
     
@@ -10,6 +9,7 @@ class Seidel:
         self.iteration=0
         self.time=0
         self.res=[]
+        self.steps=""
 
     def getSolution (self):
         return self.res
@@ -20,16 +20,37 @@ class Seidel:
     def getExcutionTime(self):
         return self.time
     
+    def getSteps(self):
+        return self.steps
+
+    def writeSolution(self, matrix, signifcantFigure):
+        self.steps += "\n"
+      
+
     def solve_with_iterations(self, num_iterations):
+        self.steps += "\n"
+        self.steps += f"Given intial guess of solution as {initial_guess}"
+        self.steps += "\n \n"
         startTime=time.time()
         for t in range(num_iterations):
+            self.steps += "\n"
+            self.steps += f"iteration # {t+1}"
+            self.steps += "\n"
             for i in range(len(self.matrixB)):
                 sum = self.matrixB[i]
+                self.steps += f"x{i} = ( {self.matrixB[i]} "
                 for j in range(len(self.matrixB)):
                     if i != j:
                         sum -= round(float(self.matrixA[i][j] )* float(self.new[j]), self.SignificantFigures)
+                        self.steps += f"+ {-1*self.matrixA[i][j]} ({self.new[j]}) "
                 self.new[i] = round(sum / self.matrixA[i][i], self.SignificantFigures)
+                self.steps += f")/{self.matrixA[i][i]} = {self.new[i]}\n"
+            self.steps+="\n"
+            
             self.old = self.new[:]
+            self.steps+="\n"
+            self.steps+=f"Solution after #{t+1} iteration is {self.old}"
+            self.steps+="\n "
         endTime=time.time()
         self.time=endTime - startTime
         self.res=self.new
@@ -40,19 +61,41 @@ class Seidel:
      tolerance /= 100
      valid = False
      iteration = 0
+     self.steps += "\n"
+     self.steps += f"Given intial guess of solution as {initial_guess}"
+     self.steps += "\n \n"
 
      while not valid:
         valid = True
         iteration += 1
+        
+        self.steps += "\n" 
+        self.steps += f"iteration # {iteration}"
+        self.steps += "\n"
+
         for i in range(len(self.matrixB)):
             sum = self.matrixB[i]
+           
+            self.steps += f"x{i} = ( {self.matrixB[i]} "
             for j in range(len(self.matrixB)):
                 if i != j:
                     sum -= round(self.matrixA[i][j] * self.new[j], self.SignificantFigures)
+                    if iteration<=10:
+                     self.steps += f"+ {-1*self.matrixA[i][j]} ({self.new[j]}) "
             self.new[i] = round(sum / self.matrixA[i][i], self.SignificantFigures)
+            
+            self.steps += f")/{self.matrixA[i][i]} = {self.new[i]}"
+            self.steps+="\n"
             relative_error = abs(self.new[i] - old[i]) / abs(self.new[i])
+           
+            self.steps+=f"   #Relative error of x{i} after {iteration} iteration = |{self.new[i]} - {old[i]} / {abs(self.new[i])}| ={relative_error}"
+            self.steps+="\n"
             if relative_error > tolerance:
                 valid = False
+       
+        self.steps+="\n"
+        self.steps+=f"Solution after #{iteration} iteration is {self.new}"
+        self.steps+="\n"
         old = self.new[:]
 
      endTime = time.time()
