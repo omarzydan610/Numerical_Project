fix significant figuers calculation for all methods

methods/decompositionsMethods/cholesky.py
@@ -28,6 +28,7 @@ class Cholesky:
         self.x = np.zeros_like(matrixB)
         self.flag = True
         self.y = np.zeros_like(self.b)
+        self.significantFigures = figures
         self.execution_time = 0
 
     def getExcutionTime(self):
@@ -42,12 +43,12 @@ class Cholesky:
         for i in self.L:
             matrixL += "|  "
             for j in i:
-                matrixL += f"{round(j, 3)}  "
+                matrixL += f"{SFCalc(j, 3)}  "
             matrixL += "|\n        "
         for i in np.transpose(self.L):
             matrixU += "|  "
             for j in i:
-                matrixU += f"{round(j, 3)}  "
+                matrixU += f"{SFCalc(j, 3)}  "
             matrixU += "|\n        "
         steps = f"""
 the equation is:
@@ -110,4 +111,6 @@ x vector = {self.x}
             for j in range(i + 1, self.n):
                 sumVar += self.L[j, i] * self.x[j]
             self.x[i] = (self.y[i] * 1.0 - sumVar) * 1.0 / self.L[i, i]
+        self.x = [SFCalc(element, self.significantFigures) for element in self.x]
+        self.y = [SFCalc(element, self.significantFigures) for element in self.y]
         self.execution_time = time.perf_counter_ns() - start_time

methods/decompositionsMethods/crout.py
@@ -28,6 +28,7 @@ class Crout:
         self.U = np.zeros_like(matrixA)
         self.y = np.zeros_like(self.b)
         self.execution_time = 0
+        self.significantFigures = figures
     
     def getExcutionTime(self):
         return self.execution_time
@@ -41,12 +42,12 @@ class Crout:
         for i in self.L:
             matrixL += "|  "
             for j in i:
-                matrixL += f"{round(j, 3)}  "
+                matrixL += f"{SFCalc(j, 3)}  "
             matrixL += "|\n        "
         for i in self.U:
             matrixU += "|  "
             for j in i:
-                matrixU += f"{round(j, 3)}  "
+                matrixU += f"{SFCalc(j, 3)}  "
             matrixU += "|\n        "
         steps = f"""
 the equation is:
@@ -107,6 +108,8 @@ x vector = {self.x}
             sumVar = 0
             for j in range(i + 1, self.n):
                 sumVar += self.U[i, j] * self.x[j]
-            self.x[i] = (self.y[i]  - sumVar) 
+            self.x[i] = (self.y[i]  - sumVar)
+        self.x = [SFCalc(element, self.significantFigures) for element in self.x]
+        self.y = [SFCalc(element, self.significantFigures) for element in self.y]
         self.execution_time = time.perf_counter_ns() - start_time
     

methods/decompositionsMethods/doolittle.py
@@ -27,6 +27,7 @@ class Doolittle:
         self.y = np.zeros_like(self.b)  
         self.x = np.zeros_like(self.b)  
         self.execution_time = 0
+        self.significantFigures = figures
 
     def getSteps(self):
         matrixL = ""
@@ -34,12 +35,12 @@ class Doolittle:
         for i in self.L:
             matrixL += "|  "
             for j in i:
-                matrixL += f"{round(j, 3)}  "
+                matrixL += f"{SFCalc(j, 3)}  "
             matrixL += "|\n        "
         for i in self.U:
             matrixU += "|  "
             for j in i:
-                matrixU += f"{round(j, 3)}  "
+                matrixU += f"{SFCalc(j, 3)}  "
             matrixU += "|\n        "
         steps = f"""
 the equation is:
@@ -97,6 +98,7 @@ x vector = {self.x}
             for j in range(i + 1, self.n):  # Replace sum with loop
                 sum_var += self.U[i, j] * self.x[j]
             self.x[i] = (self.y[i] - sum_var) / self.U[i, i]
-
+        self.x = [SFCalc(element, self.significantFigures) for element in self.x]
+        self.y = [SFCalc(element, self.significantFigures) for element in self.y]
         self.execution_time = time.perf_counter_ns() - start_time
         